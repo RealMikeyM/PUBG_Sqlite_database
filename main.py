f78f1323a0ffe794e89f9c689c29bf6cc06019de
RealMikeyM <michaelm5192@gmail.com>
2018-04-18 01:46:38 -0700

    Added some functions for the menu and scoring display
    Prints the scores of players in various ways and some bug fixes

--- main.py
+++ main.py
@@ -1,172 +1,292 @@
-import game_creation
+import os
+import sys
+import competition
 import utility_functions
-import table_creation
+import subprocess as sub
+import test
+import display_graphs
 
-def run_games(curr, conn):
-    """
-    Runs all games for each event. In this version, alll players play every event, but this may have to be changed
-    :param: conn [sqlite3.connection] -- connection to the db
-    :param: curr [sqlite3.cursor] -- cursor in the db
-    """
-    get_events = """
-    SELECT event_id, num_teams FROM Events
-    """
-    events = curr.execute(get_events).fetchall()
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def update_scores(event_id, curr, conn):
-    find_team_ids = """
-    SELECT team_id, score FROM PlayerStats
-    WHERE event_id=?
-    """
-    curr.execute(find_team_ids, (event_id,))
-    team_scores = curr.fetchall()
-    for item in team_scores:
-        team_id = item[0]
-        score = item[1]
-        # no records exist for this team, must insert new record
-        insert = """
-        INSERT INTO TeamScores(team_id, event_id, score)
-        VALUES(?, ?, ?)
-        """
-        curr.execute(insert, (team_id, event_id, score))
-        conn.commit()
+def wait():
+    return isinstance(input('Press Enter to continue...'), str)
 
-            
-def run_competition(event, curr, conn):
-    event_name = event[0]
-    # get events for the competition
-    find_event_rounds = """
-    SELECT * FROM Events
-    WHERE event_name=?
-    """
-    curr.execute(find_event_rounds, (event_name,))
-    events_list = curr.fetchall()
+def display_title():
+    clear()
+    title = """
+    ██████╗ ██╗   ██╗██████╗  ██████╗                                                      
+    ██╔══██╗██║   ██║██╔══██╗██╔════╝                                                      
+    ██████╔╝██║   ██║██████╔╝██║  ███╗                                                     
+    ██╔═══╝ ██║   ██║██╔══██╗██║   ██║                                                     
+    ██║     ╚██████╔╝██████╔╝╚██████╔╝                                                     
+    ╚═╝      ╚═════╝ ╚═════╝  ╚═════╝                                                      
+                                                                                        
+    ██████╗ ██████╗ ███╗   ███╗██████╗ ███████╗████████╗██╗████████╗██╗ ██████╗ ███╗   ██╗
+    ██╔════╝██╔═══██╗████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██║╚══██╔══╝██║██╔═══██╗████╗  ██║
+    ██║     ██║   ██║██╔████╔██║██████╔╝█████╗     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║
+    ██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██╔══╝     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║
+    ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ███████╗   ██║   ██║   ██║   ██║╚██████╔╝██║ ╚████║
+    ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚══════╝   ╚═╝   ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                                        
 
-    user_id_min = 1
-    user_id_max = 100
+                                                                
+    """
+    print(title)
+    return wait()
     
 
-    #round 1
-    for event in events_list[0:3]:
-        event_id = event[0]
-        num_teams = event[4]
+def display_top_menu():
+    clear()
+    menu = """
+    Main Menu
 
-        select_players = """
-        SELECT * FROM Players
-        WHERE user_id >= ? AND user_id <= ?
-        """
-        curr.execute(select_players, (user_id_min, user_id_max))
-        players = curr.fetchall()
+        1. Run New Competition
+        2. Modify, or Delete Records
+        3. Display Players
+        4. Display Events
+        5. Display Winners
+        6. Look Up Player ID
+        7. Display Graphs
+        8. Display Player Stats
+        9. Exit
 
-        game_creation.new_game(event_id, num_teams, players, curr, conn)
-
-        user_id_min += 100
-        user_id_max += 100
-
-        update_scores(event_id, curr, conn)
+    Select an option: """
+    print(menu, end='')
     
-    utility_functions.print_table("SELECT * FROM PlayerStats", 'Player scores after 3 rounds', curr)
+def handle_choice(conn, curr):
+    while True:
+        try:
+            choice = int(input())
+            if choice < 1 or choice > 9:
+                raise ValueError
+            else:
+                break
+        except:
+            print('The choice is not valid, choose again. ')
+    
+    if choice == 1:
+        return run_new_competition(conn, curr)
 
+    elif choice == 2:
+        return modify_record(conn, curr)
 
-    ################# round 2
-    for event in events_list[3:5]:
-        # grab top 200 from playerstats
-        event_id = event[0]
-        num_teams = event[4]
+    elif choice == 3:
+        return display_players(conn, curr)
 
-        if event[3] == 1:  # if game_number == 1
-            top_teams = """
-            SELECT * FROM Players
-            WHERE user_id IN(
-                SELECT user_id FROM Teams
-                WHERE team_id IN (
-                    SELECT team_id FROM TeamScores
-                    WHERE event_id IN (
-                        SELECT event_id FROM Events
-                        WHERE event_name=? AND round=1
-                    )
-                    ORDER BY score DESC LIMIT 100
-                )
-            )
-            """
-            utility_functions.print_table(top_teams, 'after round 1 -- top half of top 200', curr, args=(event_name,))
+    elif choice == 4:
+        return display_events(conn, curr)
+
+    elif choice == 5:
+        return display_winners(conn, curr)
+
+    elif choice == 6:
+        return look_up(conn, curr)
+
+    elif choice == 7:
+        return display_graphs.run_dash(conn, curr)
+    
+    elif choice == 8:
+        return player_stats(conn, curr)
+
+    else:
+        return False
+
+def display_player_stats_menu():
+    clear()
+    menu = """
+    Player Statistics
+
+        1. All Player Stats
+        2. Player Stats by Event ID
+        3. Top Scores by Event
+        4. Finalists
+        5. Exit to Main Menu
+    
+    Select an Option: """
+    print(menu, end='')
+
+def player_stats(conn, curr):
+    while True:
+        display_player_stats_menu()
+        while True:
+            try:
+                choice = int(input())
+                if choice < 1 or choice > 5:
+                    raise ValueError
+                else:
+                    break
+            except:
+                print('The choice is not valid, choose again: ', end='')
+
+        if choice == 1:
+            clear()
+            utility_functions.player_stats_by_all(curr)
+            wait()
+        elif choice == 2:
+            clear()
+            utility_functions.player_stats_by_event_id(curr)
+            wait()
+        elif choice == 3:
+            clear()
+            utility_functions.player_stats_by_top_scores(curr)
+            wait()
+        elif choice == 4:
+            clear()
+            utility_functions.player_stats_by_finals(curr)
+            wait()
         else:
-            top_teams = """
-            SELECT * FROM Players
-            WHERE user_id IN(
-                SELECT user_id FROM Teams
-                WHERE team_id IN (
-                    SELECT team_id FROM TeamScores
-                    WHERE event_id IN (
-                        SELECT event_id FROM Events
-                        WHERE event_name=? AND round=1
-                    )
-                    ORDER BY score DESC LIMIT 100 OFFSET 100
-                )
-            )
-            """
-            utility_functions.print_table(top_teams, 'after round 1 -- bottom half of top 200', curr, args=(event_name,))
+            return True
 
-        curr.execute(top_teams, (event_name,))
-        players = curr.fetchall()
 
-        game_creation.new_game(event_id, num_teams, players, curr, conn)
-        update_scores(event_id, curr, conn)
+def run_new_competition(conn, curr):
+    clear()
+    print('Running new Competition...')
+    competition.main_code(conn, curr, 25)
+    print('Finished')
+    return wait()
 
-    ########### round 3
-    top_100 = """
-    SELECT * FROM Players
-    WHERE user_id IN(
-        SELECT user_id FROM Teams
-        WHERE team_id IN (
-            SELECT team_id FROM TeamScores
-            WHERE event_id IN (
-                SELECT event_id FROM Events
-                WHERE event_name=? AND round=2
-            )
-            ORDER BY score DESC LIMIT 100 OFFSET 100
-        )
-    )
-    """
-    utility_functions.print_table(top_100, 'Finalists,  top 100 from round 2', curr, args=(event_name,))
-    curr.execute(top_100, (event_name,))
-    players = curr.fetchall()
-    
-    ## this needs to be generalized
-    event = events_list[5]
-    event_id = event[0]
-    num_teams = event[4]
+def display_modify_menu():
+    clear()
+    menu = """
+    Modify Record
 
-    game_creation.new_game(event_id, num_teams, players, curr, conn)
-    update_scores(event_id, curr, conn)
+        1. Delete by player ID
+        2. Modify by Player ID
+        3. Return to Main Menu
 
-    finalists = """
-    SELECT * FROM PlayerStats
-    WHERE event_id=6
-    """
-    utility_functions.print_table(finalists, 'Top 100 FINALISTS', curr)
-    #utility_functions.print_table('SELECT * FROM TeamScores', 'teamScores', curr)
+    Select an option: """
+    print(menu, end='')
+
+def modify_record(conn, curr):
+    while True:
+        display_modify_menu()
+        while True:
+            try:
+                choice = int(input())
+                if choice < 1 or choice > 3:
+                    raise ValueError
+                else:
+                    break
+            except:
+                print('The choice is not valid, choose again: ', end='')
+        
+        if choice == 1:
+            clear()
+            print('Deleting player by ID\n')
+            utility_functions.delete_player_by_id(conn, curr)
+            wait()
+
+        elif choice == 2:
+            clear()
+            utility_functions.update_player_by_id(conn, curr)
+            wait()
+        else:
+            return True
+
+def display_players_menu():
+    clear()
+    menu = """
+    Display Player Information
+
+        1. Players Alphabetically
+        2. Players by Player ID
+        3. Players by Event
+        4. Male Players
+        5. Female Players
+        6. Return to Main Menu
+
+    Select an option: """
+    print(menu, end='')
+
+def display_players(conn, curr):
+    while True:
+        display_players_menu()
+        while True:
+            try:
+                choice = int(input())
+                if choice < 1 or choice > 6:
+                    raise ValueError
+                else:
+                    break
+            except:
+                print('The choice is not valid, choose again: ', end='')
+        
+        if choice == 1:
+            clear()
+            utility_functions.display_player_by_name(curr)
+            wait()
+
+        elif choice == 2:
+            clear()
+            utility_functions.list_players(curr)
+            wait()
+
+        elif choice == 3:
+            clear()
+            utility_functions.players_by_event(curr)
+            wait()
+        
+        elif choice == 4:
+            clear()
+            utility_functions.male_players(curr)
+            wait()
+        
+        elif choice == 5:
+            clear()
+            utility_functions.female_players(curr)
+            wait()
+
+        else:
+            return True
+        
+def display_events(conn, curr):
+    clear()
+    utility_functions.list_events(curr)
+    return wait()
+
+def display_winners(conn, curr):
+    clear()
+    print('Winners by Event\n')
+    utility_functions.winners_by_event(curr)
+    return wait()
+
+def look_up(conn, curr):
+    clear()
+    print('Look up Player by ID\n')
+    while True:
+        try:
+            name = str(input('Enter the player\'s full name: ')).title()
+            if len(name.split()) != 2:
+                print('\nPlayer name must be a first name and last name. Ex: Bob Dylan')
+                raise ValueError
+            age = int(input('Enter the player\'s age: '))
+            event_id = int(input('Enter the event ID the player participated in: '))
+            utility_functions.lookup_id(name, event_id, age, curr)
+            break
+        except ValueError:
+            print('Please enter information which is correctly formatted\n')
+            
+    return wait()
 
 def main():
-    # get connection
-    conn, curr = utility_functions.connect('pubg_game_db.sqlite3')
-
-    # list all events and the team numbers associated with each event
-    events = [('ErangelSolo', 100)]
-
-    # create the tables
-    table_creation.create_tables(events, curr, conn)
-
-    players_used = 0
-    #run competition
-    for event in events:
-        run_competition(event, curr, conn)
-
-    # run all utility functions (so far)
-    #utility_functions.run_all(curr, conn)
-
-    # close the connection
-    conn.close()
+    conn, curr = utility_functions.connect()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-t':
+            clear()
+            test.testing(curr)
+        else:
+            print('Arguments not recognized, terminating program.')
+            return
+    else:
+        competition.main_code(conn, curr, 25)
+        display_title()
+        
+        while True:
+            display_top_menu()
+            if not handle_choice(conn, curr):
+                conn.close()
+                break
 
 if __name__ == '__main__':
     main()
--- utility_functions.py
+++ utility_functions.py
@@ -2,6 +2,22 @@ import sqlite3 as sql
 import table_creation
 from pathlib import Path
 
+def connect():
+    """
+    Provides a connection to the sqlite3 database
+    :param: db_name [str] -- name of the sqlite3 database to connect to
+    """
+    path = Path(Path.cwd()) / Path("Files") / "pubg_game_db.sqlite3"
+    conn = sql.connect(str(path))
+    curr = conn.cursor()
+    return conn, curr
+
+def display_playerstats(curr):
+    cmd = """
+    SELECT * FROM PlayerStats
+    """
+    print_table(cmd, 'Display Player Stats', curr)
+
 def display_player_by_name(curr):
     """
     Displays players by name
@@ -19,6 +35,7 @@ def list_players(curr):
     """
     cmd = """
     SELECT * FROM Players
+    LIMIT 300
     """
     print_table(cmd, 'Display all Players', curr)
 
@@ -50,7 +67,7 @@ def list_events(curr):
     :param: curr [sqlite3.cursor] -- cursor in the db
     """
     cmd = """
-    SELECT event_name FROM Events
+    SELECT event_id, event_name, round, game_number FROM Events
     """
     print_table(cmd, 'Display all Events', curr)
 
@@ -59,56 +76,286 @@ def players_by_event(curr):
     Displays all players by event
     :param: curr [sqlite3.cursor] -- cursor in the db
     """
-    pass
+    records = list()
 
-def winners_by_event(curr):
+    cmd = """
+    SELECT DISTINCT Players.user_id, Players.first_name, Players.last_name, Events.event_id, Events.round
+    FROM Players JOIN Teams ON Players.user_id = Teams.user_id
+    JOIN Events ON Teams.event_id = Events.event_id
+    WHERE Events.round = 1;
+    """
+    records.append(print_table(cmd, 'Players by Event:Round 1', curr))
+
+    cmd = """
+    SELECT DISTINCT Players.user_id, Players.first_name, Players.last_name, Events.event_id, Events.round
+    FROM Players JOIN Teams ON Players.user_id = Teams.user_id
+    JOIN Events ON Teams.event_id = Events.event_id
+    WHERE Events.round = 2;
+    """
+    records.append(print_table(cmd, 'Players by Event: Round 2', curr))
+
+    cmd = """
+    SELECT DISTINCT Players.user_id, Players.first_name, Players.last_name, Events.event_id, Events.round
+    FROM Players JOIN Teams ON Players.user_id = Teams.user_id
+    JOIN Events ON Teams.event_id = Events.event_id
+    WHERE Events.round = 3;
+    """
+    records.append(print_table(cmd, 'Players by Event: Round 3', curr))
+
+    return tuple(records)
+
+def winners_by_event(curr):   ##### TODO #####
     """
     Displays all winners of each event (uses award table)
     :param: curr [sqlite3.cursor] -- cursor in the db
     """
-    pass
+    cmd = """
+    SELECT Awards.event_id, Events.event_name, Awards.place, Awards.description, TeamScores.team_id, TeamScores.score
+    FROM TeamScores JOIN Events ON TeamScores.event_id = Events.event_id
+    JOIN Awards ON Awards.event_id = Events.event_id
+    LIMIT 3
+    """
+    cmd = """
+    SELECT Awards.event_id, Awards.place, Awards.description, Awards.team_id, TeamScores.score
+    FROM TeamScores JOIN Awards ON TeamScores.team_id = Awards.team_id
+    WHERE TeamScores.event_id = 6
+    """
+    print_table(cmd, 'Winners by event', curr)
 
-def lookup_id(name: str, event: str, age: int, curr):
+def lookup_id(name: str, event: int, age: int, curr):    ##### TODO ######
     """
     Looks up id by name, event, and age
     :param: name [str] -- first and last name of Player to look up
-    :param: event [str] -- event the player participated in
+    :param: event [int] -- event the player participated in
     :param: age [int] -- age of player
     :param: curr [sqlite3.cursor] -- cursor in the db
     """
     first_name = name.split()[0]
     last_name = name.split()[1]
     cmd = """
-    SELECT user_id FROM Players 
+    SELECT user_id FROM Players
     WHERE first_name=? AND last_name=? AND age=?
     AND ? IN (
-        SELECT event_name FROM Events
+        SELECT event_id FROM Events
         )
     """
-    print_table(cmd, 'Display all Events', curr, args=(first_name, last_name, age, event))
+    print_table(cmd, 'Display Player ID', curr, args=(first_name, last_name, age, event))
 
-def run_all(curr, conn):
+def delete_player_by_id(conn, curr):
     """
-    Runs all necessary functions. mostly used for utility and testing
+    removes a player from the player table.
     :param: conn [sqlite3.connection] -- connection to the db
     :param: curr [sqlite3.cursor] -- cursor in the db
     """
-    display_player_by_name(curr)
-    list_players(curr)
-    male_players(curr)
-    female_players(curr)
-    list_events(curr)
+    user_id = 0
+    while True:
+        try:
+            user_id = int(input('Enter the user id: '))
+            break
+        except ValueError:
+            print('Please enter an ID which is a number')
 
+    check = """
+    select user_id, first_name, last_name from players
+    where user_id=?
+    """
+    print_table(check, 'The User to be deleted', curr, args=(user_id,))
+
+    while True:
+        try:
+            choice = str(input('Is this the correct user? y/n ')).lower()
+            if choice != 'n' and choice != 'y':
+                raise ValueError
+            else:
+                if choice == 'y':
+                    cmd = """
+                    DELETE FROM Players WHERE user_id=?
+                    """
+                    curr.execute(cmd, (user_id,))
+                    conn.commit()
+                    return
+                else:
+                    print('Returning to menu')
+                    return
+
+        except ValueError:
+            print('Please enter y/n')
+
+def get_user(curr):
+    user_id = 0
+    while True:
+        while True:
+            try:
+                user_id = int(input('Enter user id: '))
+                break
+            except:
+                print('Please enter a correct value')
+        check = """
+        select user_id,username, first_name, last_name, gender, phone, age from players
+        where user_id=?
+        """
+        curr.execute(check, (user_id,))
+        records = curr.fetchall()
+        for rec in records:
+            if rec[1] != "":
+                print_table(check, 'The User to be changed', curr, args=(user_id,))
+                while True:
+                    try:
+                        choice = str(input('Is this the correct user? y/n')).lower()
+                        if choice != 'n' and choice != 'y':
+                            raise ValueError
+                        else:
+                            if choice == 'y':
+                                return user_id
+                            else:
+                                break
+                    except:
+                        print('Please enter y or n')
+        if len(records) == 0:
+            print("Invalid entry.")
+
+def update_player_by_id(conn, curr):
+    """
+    Updates player information
+    :param: conn [sqlite3.connection] -- connection to the db
+    :param: curr [sqlite3.cursor] -- cursor in the db
+    """
+    user_id = get_user(curr)
+    menu = """
+    Which Attribute would you like to change?
+        1. Username
+        2. First Name
+        3. Last Name
+        4. Phone
+        5. Gender
+        6. Age
+        7. Finish
+    """
+
+    while True:
+        print(menu)
+        try:
+            choice = int(input('Attribute: '))
+            if choice < 1 or choice > 7:
+                raise ValueError('Bad choice')
+            else:
+                attribute_change = ''
+                if choice == 1:
+                    attribute_change = 'username="{}"'.format(input('New Username: '))
+                elif choice == 2:
+                    attribute_change = 'first_name="{}"'.format(input('New First Name: '))
+                elif choice == 3:
+                    attribute_change = 'last_name="{}"'.format(input('New Last Name: '))
+                elif choice == 4:
+                    num = int(input('New Phone (###-###-####)').replace('-', ''))
+                    while num < 1000000000 or num > 9999999999:
+                        num = int(input("Please input a valid number: ").replace('-', ''))
+                    attribute_change = 'phone={}'.format(num)
+                elif choice == 5:
+                    attribute_change = 'gender="{}"'.format(input('New Gender (Male / Female)').title())
+                elif choice == 6:
+                    attribute_change = 'age={}'.format(int(input('New Age (>18)')))
+                else:
+                    break
+
+                cmd = """
+                UPDATE Players
+                SET {}
+                WHERE user_id=?
+                """.format(attribute_change)
+                curr.execute(cmd, (user_id,))
+                conn.commit()
+                show_change = """
+                select user_id,username, first_name, last_name, gender, phone, age from players
+                where user_id=?
+                """
+                print_table(show_change, 'Modified user', curr, args=(user_id,))
+
+        except ValueError:
+            print('Please enter a valid number')
+        except Exception as e:
+            print(str(e))
+        
+def player_info_by_id(curr):
+    """
+    Looks up Player information by player id: 
+    :param: curr [sqlite3.cursor] -- cursor in the db
+    """
+    user_id = int(input('Lookup info by ID. Enter Player ID: '))
+    cmd = """
+    SELECT * from Players
+    WHERE user_id=?
+    """
+    print_table(cmd, 'Player id '.format(user_id), curr)
+
+def player_stats_by_id(curr):
+    """
+    Looks up player stats by player id
+    :param: curr [sqlite3.cursor] -- cursor in the db
+    """
+    user_id = int(input('Lookup info by ID. Enter Player ID: '))
+    cmd = """
+    SELECT * FROM PlayerStats WHERE user_id=?
+    """
+    curr.execute(cmd, (user_id,))
+
+def player_awards_by_id(curr):   ##### TODO #####
+    """
+    Prints out the awards of a given player. If they did not place, instead of printing out the table, it prints the message " did not win anything..."
+    :param: curr [sqlite3.cursor] -- cursor in the db
+    """
+    user_id = int(input('Lookup info by ID. Enter Player ID: '))
+    cmd = """
+    SELECT * FROM Awards WHERE user_id=?
+    """
+
+def player_stats_by_all(curr):
+    """
+    """
+    cmd = """
+    SELECT * FROM PlayerStats
+    """
+    print_table(cmd, "All Player Stats", curr)
+
+def player_stats_by_event_id(curr):
+    """
+    """
+    event = input("Which event are you looking up? ")
+    while int(event) < 1 or int(event) > 6:
+        event = input("Enter an event 1-6: ")
+    cmd = """
+    SELECT * FROM PlayerStats WHERE event_id=?
+    """
+    print_table(cmd, "Player Stats EVENT {}".format(event), curr, args=(event,))
+
+def player_stats_by_top_scores(curr):
+    """
+    """
+    event = input("Which event are you looking up? ")
+    while int(event) < 1 or int(event) > 6:
+        event = input("Enter an event 1-6: ")
+    cmd = """
+    SELECT * FROM PlayerStats WHERE event_id=? ORDER BY score DESC LIMIT 10
+    """
+    print_table(cmd, "Top 10 Player Stats EVENT {}".format(event), curr, args=(event,))
+
+def player_stats_by_finals(curr):
+    """
+    """
+    cmd = """
+    SELECT * FROM PlayerStats WHERE event_id=6 ORDER BY score DESC
+    """
+    print_table(cmd, "FINALISTS", curr)
 
 def print_table(cmd, table_name, curr, args=()):
     """
     Prints a table in a readable format
-    :param: cmd [str] -- an sqlite3 command 
+    :param: cmd [str] -- an sqlite3 command
     :param: table_name [str] -- Title to display before the table is printed
     :param: curr [sqlite3.cursor] -- cursor in the db
     :param: args [tuple] -- any optional arguments to be passed to the cmd string
     """
-    
+
     # find table
     if args == ():
         curr.execute(cmd)
@@ -123,23 +370,25 @@ def print_table(cmd, table_name, curr, args=()):
         column_names.append(record[0])
 
     max_column_width = 0
-    
+
     # find max column width
     for i, column in enumerate(column_names):
         max_column_width = max(max_column_width, len(column))
         for result in results:
             max_column_width = max(max_column_width, len(str(result[i])))
-    
+
 
     print_headers(column_names, max_column_width)
-    
+
     # print the information for each table
     for record in results:
         for item in record:
             print('|{:<{width}}'.format(item, width=max_column_width), end='')
         print('|')
-    
+
     print()
+
+    return results
 
 def print_headers(column_names, max_column_width):
     """
@@ -175,14 +424,3 @@ def print_all(curr):
             SELECT * FROM {}
             """.format(table[0])
             print_table(cmd, table[0], curr)
-    
-    
-def connect(db_name):
-    """
-    Provides a connection to the sqlite3 database
-    :param: db_name [str] -- name of the sqlite3 database to connect to
-    """
-    data_dir = Path.cwd() / Path('Files') / '{}'.format(db_name)
-    conn = sql.connect(str(data_dir))
-    curr = conn.cursor()
-    return conn, curr
